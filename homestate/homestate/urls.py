90e49e7b9d0bdc04c009f5522af044624d11db2b
Chad Lindstrom <chad.lindstrom@gmail.com>
2016-01-16 19:08:07 -0800

    simple REST api for feature parity between nodejs project and django project

--- homestate/homestate/urls.py
+++ homestate/homestate/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/v1/person$', csrf_exempt(views.people)),
-    url(r'^api/v1/action$', views.getAllActions),
-    url(r'^api/v1/automatable$', views.getAllAutomatables),
+    url(r'^api/v1/action$', csrf_exempt(views.action)),
+    url(r'^api/v1/automatable$', csrf_exempt(views.automatable)),
 ]
--- homestate/homestate/views.py
+++ homestate/homestate/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, QueryDict
 from django.core import serializers
+import datetime
+
 import json
 
 from .models import Person, Action, Automatable
@@ -20,7 +22,6 @@ def getAllPeople(request):
     return JsonResponse(list(Person.objects.values('id', 'name_text')), safe=False)
 
 def createPerson(request):
-    
     name = None
     json_data = json.loads(request.body)
     try:
@@ -35,15 +36,65 @@ def createPerson(request):
     
     p = Person(name_text=name)
     p.save()
-    
     return JsonResponse({'success':1})
 
+def action(request):
+    if request.method == 'GET':
+        return getAllActions(request)
+    elif request.method == 'PUT':
+        return createAction(request)
+    return HttpResponse(status=404)
+
 def getAllActions(request):
-    actions = Action.objects.all()
-    output = ', '.join([a.name_text for a in actions])
-    return HttpResponse(output)
+    return JsonResponse(list(Action.objects.values('id', 'name_text', 'action_text')), safe=False)
+
+def createAction(request):
+    name = None
+    action = None
+    now = datetime.datetime.now()
+    
+    json_data = json.loads(request.body)
+    try:
+        name = json_data['name']
+        action = json_data['action']
+    except KeyError:
+        # not defined
+        return JsonResponse({'success':0}, status=400)
+    
+    # empty
+    if not name or not action:
+        return JsonResponse({'success':0}, status=400)
+    
+    a = Action(name_text=name,action_text=action,action_date=now)
+    a.save()
+    return JsonResponse({'success':1})
+
+def automatable(request):
+    if request.method == 'GET':
+        return getAllAutomatables(request)
+    elif request.method == 'PUT':
+        return createAutomatable(request)
+    return HttpResponse(status=404)
 
 def getAllAutomatables(request):
-    automatables = Automatable.objects.all()
-    output = ', '.join([a.name_text for a in automatables])
-    return HttpResponse(output)
+    return JsonResponse(list(Automatable.objects.values('id', 'name_text', 'state_text')), safe=False)
+
+def createAutomatable(request):
+    name = None
+    state = None
+    
+    json_data = json.loads(request.body)
+    try:
+        name = json_data['name']
+        state = json_data['state']
+    except KeyError:
+        # not defined
+        return JsonResponse({'success':0}, status=400)
+    
+    # empty
+    if not name or not state:
+        return JsonResponse({'success':0}, status=400)
+    
+    a = Automatable(name_text=name,state_text=state)
+    a.save()
+    return JsonResponse({'success':1})
